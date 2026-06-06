953203b8f254f5b1ff45227bee81172875ccb15b
saimamukherjee <saimamukerjee@gmail.com>
2026-06-06 12:43:31 +0530

    day-8 uploaded.

---
+++ day-8/halfpyramid.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int rows;
+    cout << "enter the number of rows:";
+    cin >> rows;
+    // outer loop for the number of rows 
+    for(int i = 1; i <= rows; i++){
+        for (int j= 1; j<=i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
