73176062b6d163cbb3129ba737d2171714bc1113
saimamukherjee <saimamukerjee@gmail.com>
2026-06-12 23:13:40 +0530

    uploaded.

---
+++ day-12/program4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+bool isPerfect(int n){
+    int sum = 0;
+    for( int i =1; i <= n/2; i++){
+        if( n % i== 0)
+        sum = sum + i;
+        
+    }
+    return sum == n && n >0;
+}
+int main(){
+    int n;
+    cout << "enter a number: ";
+    cin >> n;
+    if( isPerfect(n))
+        cout << n << " is a perfect number." << endl;
+    else{
+        cout << n << " is not a perfect number." << endl;
+    }
+    return 0;
+}
